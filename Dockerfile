27a7113b8329f3afe5a33b52c1855b3d0329fb4a
Jon Coulter <jon@getstellar.ai>
2023-07-28 16:56:57 -0400

    updates

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM python:3.9-alpine3.13
 LABEL maintainer="jon"
 
 ENV PYTHONUNBUFFERED 1
-ENV PYTHONDONTWRITEBYTECODE 1
 
 COPY ./requirements.txt /tmp/requirements.txt
 COPY ./requirements.dev.txt /tmp/requirements.dev.txt
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,27 @@
+"""
+Django command to wait for the database to be available
+"""
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database"""
+
+    def handle(self, *args, **options):
+        """Handle the command"""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError) as e:
+                self.stdout.write(f'Database unavailable, waiting 1s: {e}')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
